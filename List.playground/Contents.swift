929b8125e5e7547a7a15a14e7e32b1f3dad27f74
CaoDong <caodong1991@gmail.com>
2019-12-15 22:22:12 +0800

    链式存储结构单链表实现完成

--- List.playground/Contents.swift
+++ List.playground/Contents.swift
@@ -96,7 +96,7 @@ public struct SequenceList<T: Equatable> {
     
 }
 
-
+/*
 var sequenceList = SequenceList<Int>()
 // 检查是否为空
 if sequenceList.isEmpty() {
@@ -131,3 +131,217 @@ sequenceList.printList()
 // 清除
 sequenceList.clear()
 sequenceList.printList()
+*/
+
+// 链式存储结构
+// 单链表
+public class SingleListNode<T> {
+    var value: T?
+    var next: SingleListNode?
+    public init() {}
+    public init(_ value: T) {
+        self.value = value
+    }
+}
+
+public class SingleList<T> {
+    private var head: SingleListNode<T>
+    public var length: Int { // O(n)
+        guard var node = head.next else {
+            return 0
+        }
+        var count = 1
+        while let next = node.next {
+            node = next
+            count += 1
+        }
+        return count
+    }
+    
+    public var firstValue: T? { // O(1)
+        return head.next?.value
+    }
+    
+    public var lastValue: T? {
+        guard var node = head.next else {
+            return nil
+        }
+        while let next = node.next {
+            node = next
+        }
+        return node.value
+    }
+    
+    private var firstNode: SingleListNode<T>? { // O(1)
+        return head.next
+    }
+    
+    private var lastNode: SingleListNode<T>? {
+        guard var node = head.next else {
+            return nil
+        }
+        while let next = node.next {
+            node = next
+        }
+        return node
+    }
+    
+    // 初始化构造器
+    public init() {
+        head = SingleListNode<T>()
+    }
+    
+    // 判断是否为空
+    public func isEmpty() -> Bool {
+        return head.next == nil
+    }
+    
+    // 添加元素
+    public func append(_ value: T) {
+        let newNode = SingleListNode(value)
+        // 尾插法
+        if let lastNode = lastNode {
+            lastNode.next = newNode
+        } else {
+            head.next = newNode
+        }
+        
+        // 头插法
+        /*
+        if let firstNode = firstNode {
+            newNode.next = firstNode
+        }
+        head.next = newNode
+        */
+    }
+    
+    // 根据位置获取元素
+    public func getElem(at index: Int) -> T? {
+        var node = head.next
+        var loop = 1
+        while loop < index {
+            node = node?.next
+            loop += 1
+        }
+        guard node != nil else {
+            return nil
+        }
+        return node?.value
+    }
+    
+    // 插入元素
+    public func insert(_ value: T, at index: Int) {
+        var node: SingleListNode<T>? = head
+        var loop = 1
+        while loop < (index - 1) {
+            node = node?.next
+            loop += 1
+        }
+        guard node != nil else {
+            return
+        }
+        let newNode = SingleListNode(value)
+        newNode.next = node?.next
+        node?.next = newNode
+    }
+    
+    // 删除元素
+    public func remove(at index: Int) -> T? {
+        var node: SingleListNode<T>? = head.next
+        var previousNode: SingleListNode<T>? = head
+        var loop = 1
+        while loop < index {
+            node = node?.next
+            previousNode = node
+            loop += 1
+        }
+        guard node != nil else {
+            return nil
+        }
+        previousNode?.next = node?.next
+        return node?.value
+    }
+    
+    // 删除所有元素
+    public func removeAll() {
+        var node = head.next
+        var deleteNode: SingleListNode<T>? = nil
+        while node != nil {
+            deleteNode = node?.next
+            node = nil
+            node = deleteNode
+        }
+        head.next = nil
+    }
+    
+    // 打印
+    public func printfList() {
+        var listString: String = ""
+        var node = head.next
+        while node != nil {
+            listString += "\(String(describing: node!.value!))"
+            node = node!.next
+            if node != nil {
+                listString += "->"
+            }
+        }
+        if listString.isEmpty {
+            print("List is empty")
+        } else {
+            print(listString)
+        }
+    }
+}
+
+
+
+let singleList = SingleList<String>()
+singleList.isEmpty()
+singleList.firstValue
+singleList.lastValue
+singleList.getElem(at: 2)
+singleList.getElem(at: 1)
+
+
+singleList.append("hello")
+singleList.isEmpty()
+singleList.firstValue
+singleList.lastValue
+singleList.printfList()
+
+singleList.append("world")
+singleList.isEmpty()
+singleList.firstValue
+singleList.lastValue
+singleList.printfList()
+
+singleList.length
+singleList.getElem(at: 2)
+singleList.insert("Swft", at: 1)
+singleList.printfList()
+
+singleList.insert("Other", at: 10)
+singleList.printfList()
+
+singleList.remove(at: 1)
+singleList.printfList()
+
+singleList.remove(at: 5)
+singleList.printfList()
+
+singleList.removeAll()
+singleList.printfList()
+//if var firstValue: String? =  {
+//    print("no value")
+//} else {
+//    print("first is \(firstValue)")
+//}
+//
+//if var lastValue =  {
+//    print("no value")
+//} else {
+//    print("last is \(lastValue)")
+//}
+
+
+
